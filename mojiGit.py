fd8d93e780fa538c3eed3d7945d033588af8415a
ztrx67 <ztrx67@gmail.com>
2020-03-23 10:31:42 +0800

    更新pm2.5

--- mojiGit.py
+++ mojiGit.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import sys
-
+import wechat
 
 
 class begin():
@@ -12,7 +11,7 @@ class begin():
     def git_weather(self):
         url = "https://tianqi.moji.com/weather/china/"+self.shen+"/"+self.city
         html = urlopen(url).read().decode('utf-8')
-
+        #print(html)
         soup = BeautifulSoup(html,'html.parser')
 
         city = soup.select('ul>li')[3].string
@@ -40,10 +39,12 @@ class begin():
                 advise += nlist[i]+'，'
 
         advise = advise[0:-1]+'。'
-        pm25 = soup.find_all('strong', class_='level_2')[0]
-        pm25 = str(pm25).split('\r')[1].strip()
+        # pm25 = soup.find_all('strong', class_='level_2')[0]
+        # pm25 = str(pm25).split('\r')[1].strip()
+        # pm_25 = pm25.split(' ')
+        pm25 = soup.select('div>ul>li>a>span>img')[0]['alt']
+        #print(pm25)
         pm_25 = pm25.split(' ')
-
         return city+'：'+weather_all,wind,humidity,morning_weather,night_weather,advise.replace('墨迹天气','阿德天气'),'PM2.5:'+pm_25[0]+'('+pm_25[1]+')'
 
 
@@ -57,4 +58,5 @@ if __name__ == '__main__':
     print(night_weather)
     print(pm25)
     print(advise)
+
 
